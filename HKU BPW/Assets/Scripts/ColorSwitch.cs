fc3b0a50f2314c42fda5043b7910bac9e6beeaa2
RutOu <rutger.oudenampsen@student.hku.nl>
2020-03-17 15:26:44 +0100

    unity project met abyss versie 1

---
+++ HKU BPW/Assets/Scripts/ColorSwitch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorSwitch : MonoBehaviour
+{
+
+    
+
+    public Material material1;
+    public Material material2;
+    public Material material3;
+
+    private void Start()
+    {
+        Debug.Log("Color Switch!");
+    }
+
+    private void Update()
+    {
+
+    }
+
+}
---
+++ HKU BPW/Assets/Scripts/TriggerColorSwitch2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerColorSwitch2 : MonoBehaviour
+{
+
+    public GameObject Tower;
+    public Material material2;
+
+    private void OnCollisionEnter(Collision Collision)
+    {
+        //als de player collide met het switch object, dan word de boolean colorTrigger of 'true' gezet. 
+
+        if (Collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Trigger Hit!"); //om te kijken of de collision ook echt gedetecteerd wordt.
+
+            Tower.GetComponent<Renderer>().material = material2;
+            //Material Swap? 1. public Material newMaterialRef;, 2. renderer.material = newMaterialRef;
+        }
+
+    }
+}
