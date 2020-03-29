9f5453820a5f250072efc0d84f339070e517a81f
RutOu <rutger.oudenampsen@student.hku.nl>
2020-03-30 00:09:37 +0200

    Rest of Game until uploaded to canvas

---
+++ HKU BPW/Assets/Scripts/CutsceneTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneTrigger : MonoBehaviour
+{
+    public GameManager GameManager;
+
+    private void OnCollisionEnter(Collision Collision)
+    {
+        if (Collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Trigger Hit!"); //om te kijken of de collision ook echt gedetecteerd wordt.
+            GameManager.TriggerCutscene();
+        }
+
+        
+    }
+}
---
+++ HKU BPW/Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    bool cutSceneHasTriggered = false;
+
+    public GameObject CutsceneTimeline;
+
+    public void TriggerCutscene()
+    {
+        CutsceneTimeline.SetActive(true);
+    }
+}
